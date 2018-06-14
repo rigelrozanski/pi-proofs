d0c8f7b80130d4caf66c9507266fb7598ed228db
rigelrozanski <rigel.rozanski@gmail.com>
2018-06-13 17:52:58 -0700

    fixes

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// nolint
 package main
 
 import (
@@ -5,60 +6,88 @@ import (
 	"math/big"
 )
 
+func Mul(r1, r2 big.Float) big.Float {
+	var z *big.Float = big.NewFloat(1)
+	return *z.Mul(&r1, &r2)
+}
+
+func Quo(r1, r2 big.Float) big.Float {
+	var z *big.Float = big.NewFloat(1)
+	return *z.Quo(&r1, &r2)
+}
+
+func Add(r1, r2 big.Float) big.Float {
+	var z *big.Float = big.NewFloat(1)
+	return *z.Add(&r1, &r2)
+}
+
+func Sub(r1, r2 big.Float) big.Float {
+	var z *big.Float = big.NewFloat(1)
+	return *z.Sub(&r1, &r2)
+}
+
+func Sqrt(r1 big.Float) big.Float {
+	var z *big.Float = big.NewFloat(1)
+	return *z.Sqrt(&r1)
+}
+
+func Pow2(r1 big.Float) big.Float {
+	var z *big.Float = big.NewFloat(1)
+	return *z.Mul(&r1, &r1)
+}
+
 func main() {
 
-	r := big.NewFloat(1) // unit circle radius
-
-	A := big.NewFloat(2) // A0 = area of the inner square
+	one := *big.NewFloat(1)
+	two := *big.NewFloat(2)
+	r := *big.NewFloat(1) // unit circle radius
+	A := *big.NewFloat(2) // A0 = area of the inner square
 
 	// length of the inner side (start with the side length of the square)
-	var z, z2, z3 *big.Float = big.NewFloat(1), big.NewFloat(1), big.NewFloat(1)
-	two := big.NewFloat(2)
-	sqrt2 := z.Sqrt(two)
+	sqrt2 := Sqrt(two)
 	Ls := sqrt2 // length side
+	fmt.Printf("debug Ls: %v\n", &Ls)
 
 	// inner length
-	Li := z.Quo(big.NewFloat(1), sqrt2) // 1/sqrt(2)
+	Li := Quo(one, sqrt2) // 1/sqrt(2)
+	fmt.Printf("debug Li: %v\n", &Li)
 
 	// area of the big curve to the area of the under triangle segment
-	curve := z.Quo(big.NewFloat(5), big.NewFloat(7))
+	//curve := Quo(*big.NewFloat(6), *big.NewFloat(7))
+	curve := Quo(*big.NewFloat(1), *big.NewFloat(3))
+	fmt.Printf("debug curve: %v\n", &curve)
 
 	// number of segments
-	n := big.NewFloat(4)
+	n := *big.NewFloat(4)
 
-	for i := 0; i < 2; i++ {
+	for i := 0; i < 100; i++ {
 		fmt.Printf("debug i: %v\n", i)
 
 		// height
-		h := z.Sub(r, Li)
+		h := Sub(r, Li)
+		//fmt.Printf("debug h: %v\n", &h)
 
-		halfLs := z.Quo(Ls, two)
-		fmt.Printf("debug halfLs: %v\n", halfLs)
-		Atriangle := z.Quo(halfLs, h)
-		fmt.Printf("debug Atriangle: %v\n", Atriangle)
-		Atriangles := z.Mul(Atriangle, n)
-		fmt.Printf("debug Atriangles: %v\n", Atriangles)
-		Aarcs := z.Mul(Atriangles, curve)
-		fmt.Printf("debug Aarcs: %v\n", Aarcs)
+		halfLs := Quo(Ls, two)
+		//fmt.Printf("debug halfLs: %v\n", &halfLs)
+		Atriangle := Mul(halfLs, h)
+		//fmt.Printf("debug Atriangle: %v\n", &Atriangle)
+		Atriangles := Mul(Atriangle, n)
+		//fmt.Printf("debug Atriangles: %v\n", &Atriangles)
+		Aarcs := Mul(Atriangles, curve)
+		//fmt.Printf("debug Aarcs: %v\n", &Aarcs)
 
-		A = z.Add(A, Atriangles)
-		AIterationRes := z.Add(A, Aarcs)
+		A = Add(A, Atriangles)
+		AIterationRes := Add(A, Aarcs)
 
-		fmt.Printf("i: %v, res %v\n", i, AIterationRes)
+		fmt.Printf("i: %v, res %v\n", i, &AIterationRes)
 
 		// set the length and number of segments for next time
-		n = n.Mul(n, two)
+		n = Mul(n, two)
 
-		z = z.Mul(h, h)
-		z2 = z2.Mul(halfLs, halfLs)
-		z3 = z3.Add(z, z2)
-		Ls = z.Sqrt(z3)
+		Ls = Sqrt(Add(Pow2(h), Mul(halfLs, halfLs)))
 
-		halfLs = z.Quo(Ls, two)
-		z2 = z2.Mul(halfLs, halfLs)
-		fmt.Printf("debug z2: %v\n", z2)
-		z3 = z3.Sub(r, z2)
-		fmt.Printf("debug z3: %v\n", z3)
-		Li = z.Sqrt(z3)
+		halfLs = Quo(Ls, two)
+		Li = Sqrt(Sub(r, Pow2(halfLs)))
+		//fmt.Printf("debug Li: %v\n", &Li)
 	}
 }
