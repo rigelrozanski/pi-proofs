5a5201f6a2a7bed9b2ec42d4279de774d037fbf2
rigelrozanski <rigel.rozanski@gmail.com>
2018-06-15 16:03:45 -0700

    added precision

--- main.go
+++ main.go
@@ -6,62 +6,76 @@ import (
 	"math/big"
 )
 
+const prec = 100
+
 func Mul(r1, r2 big.Float) big.Float {
 	var z *big.Float = big.NewFloat(1)
+	z.SetPrec(prec)
 	return *z.Mul(&r1, &r2)
 }
 
 func Quo(r1, r2 big.Float) big.Float {
 	var z *big.Float = big.NewFloat(1)
+	z.SetPrec(prec)
 	return *z.Quo(&r1, &r2)
 }
 
 func Add(r1, r2 big.Float) big.Float {
 	var z *big.Float = big.NewFloat(1)
+	z.SetPrec(prec)
 	return *z.Add(&r1, &r2)
 }
 
 func Sub(r1, r2 big.Float) big.Float {
 	var z *big.Float = big.NewFloat(1)
+	z.SetPrec(prec)
 	return *z.Sub(&r1, &r2)
 }
 
 func Sqrt(r1 big.Float) big.Float {
 	var z *big.Float = big.NewFloat(1)
+	z.SetPrec(prec)
 	return *z.Sqrt(&r1)
 }
 
 func Pow2(r1 big.Float) big.Float {
 	var z *big.Float = big.NewFloat(1)
+	z.SetPrec(prec)
 	return *z.Mul(&r1, &r1)
+}
+
+func newFloat(val float64) big.Float {
+	result := *big.NewFloat(val)
+	result.SetPrec(prec)
+	return result
 }
 
 func main() {
 
-	one := *big.NewFloat(1)
-	two := *big.NewFloat(2)
-	r := *big.NewFloat(1) // unit circle radius
-	A := *big.NewFloat(2) // A0 = area of the inner square
+	one, two, three, four, six, seven := newFloat(1), newFloat(2), newFloat(3), newFloat(4), newFloat(6), newFloat(7)
+	_, _, _, _, _, _ = one, two, three, four, six, seven
+	r := newFloat(1) // unit circle radius
+	A := newFloat(2) // A0 = area of the inner square
 
 	// length of the inner side (start with the side length of the square)
 	sqrt2 := Sqrt(two)
 	Ls := sqrt2 // length side
-	fmt.Printf("debug Ls: %v\n", &Ls)
+	//fmt.Printf("debug Ls: %v\n", &Ls)
 
 	// inner length
 	Li := Quo(one, sqrt2) // 1/sqrt(2)
-	fmt.Printf("debug Li: %v\n", &Li)
+	//fmt.Printf("debug Li: %v\n", &Li)
 
 	// area of the big curve to the area of the under triangle segment
-	//curve := Quo(*big.NewFloat(6), *big.NewFloat(7))
-	curve := Quo(*big.NewFloat(1), *big.NewFloat(3))
-	fmt.Printf("debug curve: %v\n", &curve)
+	curve := Quo(six, seven)
+	//curve := Quo(*big.NewFloat(1), *big.NewFloat(3))
+	//fmt.Printf("debug curve: %v\n", &curve)
 
 	// number of segments
-	n := *big.NewFloat(4)
+	n := newFloat(4)
 
-	for i := 0; i < 100; i++ {
-		fmt.Printf("debug i: %v\n", i)
+	for i := 0; i < 1000; i++ {
+		//fmt.Printf("debug i: %v\n", i)
 
 		// height
 		h := Sub(r, Li)
